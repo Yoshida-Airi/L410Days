7c5a341cf7c75160be89f0e7560fba2072fc3b0b
Yoshida-Airi <115054911+Yoshida-Airi@users.noreply.github.com>
2025-09-07 12:07:54 +0900

    ジャンプをなんとなく作成

--- DirectXGame/GameObject/Player/Player.cpp
+++ DirectXGame/GameObject/Player/Player.cpp
@@ -1,22 +1,73 @@
+#define NOMINMAX
 #include "Player.h"
+#include<algorithm>
 
-Player::~Player() { delete sprite_; }
+using namespace KamataEngine;
+
+Player::~Player() {
+	delete sprite_;
+	delete worldTransform_;
+}
 
 void Player::Initialize() {
-	textureHandle_ = KamataEngine::TextureManager::Load("white64x64.png");
-	sprite_ = KamataEngine::Sprite::Create(textureHandle_, {status.pos});
+	textureHandle_ = TextureManager::Load("white64x64.png");
+	worldTransform_ = new WorldTransform();
+	worldTransform_->Initialize();
+	worldTransform_->translation_ = state_.pos;
+	sprite_ = Sprite::Create(textureHandle_, {worldTransform_->translation_.x, worldTransform_->translation_.y});
 }
 
 void Player::Update() {
-	KamataEngine::Vector2 position = sprite_->GetPosition();
+	Vector2 position = sprite_->GetPosition();
 
-	if (KamataEngine::Input::GetInstance()->PushKey(DIK_RIGHT)) {
-		position.x += status.speed;
-	} else if (KamataEngine::Input::GetInstance()->PushKey(DIK_LEFT)) {
-		position.x -= status.speed;
+	if (onGround_) {
+		// 移動
+		if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+			state_.velocity.x = 2.0f;
+		} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+			state_.velocity.x = -2.0f;
+		}
+		else
+		{
+			state_.velocity.x = 0.0f;
+		}
+		// ジャンプ
+		if (Input::GetInstance()->TriggerKey(DIK_UP)) {
+			state_.velocity.y = kJumpAcceleration;
+		}
+	} else { // 空中
+		state_.velocity.y -= kGravityAcceleration;
+		state_.velocity.y = std::max(state_.velocity.y, -kLimitFallSpeed);
 	}
 
-	sprite_->SetPosition(position);
+	bool landing = false;
+
+	//地面との当たり判定
+	if (state_.velocity.y < 0) {
+		if (worldTransform_->translation_.y <= 100.0f) {
+			landing = true;
+		}
+	}
+
+	if (onGround_) {
+		if (worldTransform_->translation_.y > 100.0f) {
+			onGround_ = false;
+		}
+	} else {
+		// 着地
+		if (landing) {
+			worldTransform_->translation_.y = 100.0f;
+			state_.velocity.y = 0.0f;
+			onGround_ = true;
+		}
+	}
+	worldTransform_->translation_.x += state_.velocity.x;
+	worldTransform_->translation_.y += state_.velocity.y;
+
+	float screenY = 720.0f - worldTransform_->translation_.y;
+	sprite_->SetPosition({worldTransform_->translation_.x, screenY});
+
+
 }
 
 void Player::Draw() { sprite_->Draw(); }
--- DirectXGame/GameObject/Player/Player.h
+++ DirectXGame/GameObject/Player/Player.h
@@ -7,8 +7,8 @@
 /// </summary>
 struct Status 
 {
-	KamataEngine::Vector2 pos = {64, 600}; // 初期化位置
-	float speed = 2.0f;		 // 移動速度
+	KamataEngine::Vector3 pos = {64, 100, 0};      // 初期化位置
+	KamataEngine::Vector2 velocity = {}; // 移動速度
 	int power = 1;           // 力と重さの値
 };
 
@@ -23,7 +23,14 @@ public:
 private: 
 
 	KamataEngine::Sprite* sprite_ = nullptr;
+	KamataEngine::WorldTransform* worldTransform_;
 	uint32_t textureHandle_ = 0;
-	Status status;
+	Status state_;
+
+	bool onGround_ = true;
+
+	static inline const float kJumpAcceleration = 20.0f;
+	static inline const float kGravityAcceleration = 0.98f;
+	static inline const float kLimitFallSpeed = 15.0f;
 	
 };
